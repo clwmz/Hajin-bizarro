3436e146b7725e9b38c2144b1d001c9eae56d92f
Pulin <113272580+PulinPulinho@users.noreply.github.com>
2024-02-20 09:26:07 -0300

    Atualizar o index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const nao = document.querySelector('#nao');
 const sim = document.querySelector('#sim');
+let sim_size = 10;
 
 nao.onclick = () => {
+    sim.style.width = (sim_size+1) + 'vw';
     nao.style.position = 'absolute';
     nao.style.left = '1vw';
     nao.style.top = (Math.floor(Math.random()*50) + 1) + 'vh';
