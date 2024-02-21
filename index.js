d87976e6f009e3c9d63e512b49ea4dcd9f366f6c
clwmz <hajinlimkr77@icloud.com>
2024-02-20 21:15:05 -0300

    refactor: idk why im helping u to make fun of me

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const nao = document.querySelector('#nao');
-const sim = document.querySelector('#sim');
 let sim_size = 10;
 
 nao.onclick = () => {
-    sim.style.width = (sim_size+1) + 'vw';
+    sim_size += 2;
+
+    sim.style.width = `${sim_size}vw`;
     nao.style.position = 'absolute';
     nao.style.left = '1vw';
-    nao.style.top = (Math.floor(Math.random()*50) + 1) + 'vh';
-    nao.style.left = (Math.floor(Math.random()*50) + 1) + 'vw';
-}
-sim.onclick = () => {
-    window.location = "muitobem.html"
+    nao.style.top = (Math.floor(Math.random() * 50) + 1) + 'vh';
+    nao.style.left = (Math.floor(Math.random() * 50) + 1) + 'vw';
 }
